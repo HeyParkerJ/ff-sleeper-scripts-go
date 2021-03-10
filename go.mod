c9158cc5b78af45cd28b93c964548d089850223c
Parker Johnson <mrstormthebeach@gmail.com>
2021-03-09 21:18:10 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/HeyParkerJ/ff-sleeper-scripts-go
+
+go 1.15
---
+++ hello_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello()
+	want := "Hello, world"
+
+	if got != want {
+		t.Errorf("git %q want %q", got, want)
+	}
+}
